5c66d6a17766c7d54e7fdce680a903140875d165
Vitaly <vitaly.kiktiov@gmail.com>
2018-11-27 10:46:02 +0200

    everything on typescript + build pipeline

---
+++ dist/batchNotificator.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('hello123');
+console.log('hello123');
+console.log('hello123');
+console.log('hello123');
+console.log('hello1234');
+//# sourceMappingURL=batchNotificator.js.map
