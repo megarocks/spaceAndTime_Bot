0b3cb2867169063b786eb780777f1ed6ebfbfea7
Vitaly <vitaly.kiktiov@gmail.com>
2018-11-27 14:05:26 +0200

    empty dist directory

--- dist/batchNotificator.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-console.log('hello123');
-console.log('hello123');
-console.log('hello123');
-console.log('hello123');
-console.log('hello1234');
-//# sourceMappingURL=batchNotificator.js.map
