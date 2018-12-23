134649cb68c58bbd749aa3d37306eb4041cddee3
Vitaly <vitaly.kiktiov@gmail.com>
2018-12-23 12:57:42 +0100

    reformat utils.ts and add solarDateNotified to Chat interface

--- src/utils.ts
+++ src/utils.ts
@@ -1,45 +1,46 @@
-import { DateTime } from 'luxon';
+import {DateTime} from 'luxon';
 
 export interface MoonDay {
-    dayNumber: number,
-    dayStart: DateTime,
-    dayEnd: DateTime
+  dayNumber: number,
+  dayStart: DateTime,
+  dayEnd: DateTime
 }
 
 export interface Chat {
-    chatId: number,
-    location: {
-        type: string,
-        coordinates: [number, number]
-    },
-    moonDayNotified: number
+  chatId: number,
+  location: {
+    type: string,
+    coordinates: [number, number]
+  },
+  moonDayNotified: number,
+  solarDateNotified: Date
 }
 
 export interface NotificationResult {
-    chatId: number,
-    moonDayNumber: number | undefined,
-    solarDate: Date
+  chatId: number,
+  moonDayNumber: number | undefined,
+  solarDate: Date
 }
 
-export function createReportMessage({moonDay, timeZone} : { moonDay: MoonDay, timeZone: string }): string {
-    if (!moonDay) return 'Не могу рассчитать лунный день. Странная астрологическая обстановка. Учти это';
+export function createReportMessage({moonDay, timeZone}: { moonDay: MoonDay, timeZone: string }): string {
+  if (!moonDay) return 'Не могу рассчитать лунный день. Странная астрологическая обстановка. Учти это';
 
-    const {dayNumber, dayStart, dayEnd} = moonDay;
-    let leftHours = Math.floor(dayEnd.setZone(timeZone).diff(DateTime.utc().setZone(timeZone), 'hours').hours)
-    let leftHoursMessage = leftHours ? `Через ${leftHours} ${getNoun(leftHours, 'час', 'часа', 'часов')}` : 'менее чем через час';
+  const {dayNumber, dayStart, dayEnd} = moonDay;
+  let leftHours = Math.floor(dayEnd.setZone(timeZone).diff(DateTime.utc().setZone(timeZone), 'hours').hours)
+  let leftHoursMessage = leftHours ? `Через ${leftHours} ${getNoun(leftHours, 'час', 'часа', 'часов')}` : 'менее чем через час';
 
 
-    let reportMessage =
-        `Текущий лунный день: *${dayNumber}*
+  let reportMessage =
+    `Текущий лунный день: *${dayNumber}*
 День начался: _${dayStart.setZone(timeZone).setLocale('ru').toLocaleString(DateTime.DATETIME_SHORT)}_
 День завершится: _${dayEnd.setZone(timeZone).setLocale('ru').toLocaleString(DateTime.DATETIME_SHORT)}_
 Начало следующего: _${leftHoursMessage}_
 `
-    return reportMessage
+  return reportMessage
 }
 
 export function createStartMessage(): string {
-    return `Привет
+  return `Привет
 Буду оповещать тебя о начале нового лунного дня и месяца, фазах луны, и других натуральных циклах нашей планеты
 Доступные команды:
 /location - задать своё местоположение
@@ -47,23 +48,23 @@ export function createStartMessage(): string {
 }
 
 export function createHelpMessage(): string {
-    return 'Пришли мне свою локацию и я скажу тебе какой в этой точке пространства сейчас лунный день\n' +
-        'Буду оповещать тебя о начале нового лунного дня, и месяца, фазах луны, и других натуральных циклах нашей планеты\n' +
-        'Если не удаётся отправить локацию, проверь в настройках, что у телеграм есть доступ к gps'
+  return 'Пришли мне свою локацию и я скажу тебе какой в этой точке пространства сейчас лунный день\n' +
+    'Буду оповещать тебя о начале нового лунного дня, и месяца, фазах луны, и других натуральных циклах нашей планеты\n' +
+    'Если не удаётся отправить локацию, проверь в настройках, что у телеграм есть доступ к gps'
 }
 
 export function getNoun(number: number, one: string, two: string, five: string): string {
-    let n = Math.abs(number);
-    n %= 100;
-    if (n >= 5 && n <= 20) {
-        return five;
-    }
-    n %= 10;
-    if (n === 1) {
-        return one;
-    }
-    if (n >= 2 && n <= 4) {
-        return two;
-    }
+  let n = Math.abs(number);
+  n %= 100;
+  if (n >= 5 && n <= 20) {
     return five;
+  }
+  n %= 10;
+  if (n === 1) {
+    return one;
+  }
+  if (n >= 2 && n <= 4) {
+    return two;
+  }
+  return five;
 }
